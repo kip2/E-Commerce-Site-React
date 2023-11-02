0e24d2db311b7e2c3291743770c053082d226d78
kip2 <tqa1no4@gmail.com>
2023-11-02 23:52:31 +0900

    add: リセットCSS追加

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import 'sanitize.css'
 import Header from './Header'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
