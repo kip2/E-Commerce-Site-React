9b5bd6d477b50af70f5cf1a5471422c0b41ed640
kip2 <tqa1no4@gmail.com>
2023-11-02 23:45:22 +0900

    fix: 色味の調整

--- app/src/Header.tsx
+++ app/src/Header.tsx
@@ -1,7 +1,7 @@
 import { FaShoppingCart } from "react-icons/fa";
 import { SlPeople } from "react-icons/sl";
 import "./Header.css";
-import IconButton from "./IconButton";
+import IconButton from "./components/atoms/IconButton";
 
 export default function Header() {
     return (
@@ -11,8 +11,8 @@ export default function Header() {
                     <h1>Electric Commerce</h1>
                 </div>
                 <div className="header-icons">
-                    <IconButton icon={<FaShoppingCart color="white" size={30}/>} />
-                    <IconButton icon={<SlPeople color="white" size={30}/>} />
+                    <IconButton icon={<FaShoppingCart color="black" size={30}/>} />
+                    <IconButton icon={<SlPeople color="black" size={30}/>} />
                 </div>
             </div>
         </>
