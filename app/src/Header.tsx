15b107c21179aee39b3200d9dd2f112fedeac6cd
kip2 <tqa1no4@gmail.com>
2023-11-02 22:36:49 +0900

    add: headerの雛形作成

---
+++ app/src/Header.tsx
@@ -0,0 +1,10 @@
+import { FaStar } from "react-icons/fa";
+
+export default function Header() {
+    return (
+        <>
+            <h1>Electric Commerce</h1>,
+            <FaStar color="red" />
+        </>
+    );
+}
