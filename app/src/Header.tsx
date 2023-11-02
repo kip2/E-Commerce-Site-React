768be5464341d3e6b4a96906db389744e6aa530d
kip2 <tqa1no4@gmail.com>
2023-11-02 23:21:56 +0900

    Headerコンポーネント完成

--- app/src/Header.tsx
+++ app/src/Header.tsx
@@ -1,13 +1,23 @@
 import { FaShoppingCart } from "react-icons/fa";
 import { SlPeople } from "react-icons/sl";
-
+import "./Header.css";
 
 export default function Header() {
     return (
         <>
-            <h1>Electric Commerce</h1>,
-            <FaShoppingCart color="yellow" />
-            <SlPeople color="yellow" />
+            <div className="header">
+                <div className="header-text">
+                    <h1>Electric Commerce</h1>
+                </div>
+                <div className="header-icons">
+                    <button>
+                        <FaShoppingCart color="white" size={30}/>
+                    </button>
+                    <button>
+                        <SlPeople color="white" size={30} />
+                    </button>
+                </div>
+            </div>
         </>
     );
 }
