8607d32f5a22e34d268537ef877b99f0a6c356bf
kip2 <tqa1no4@gmail.com>
2023-11-07 22:50:57 +0900

    todoコメントを追加

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -5,6 +5,9 @@ import 'sanitize.css'
 import Header from './components/organisms/Header'
 import ProductCard from './components/molecules/ProductCard'
 
+// todo: ProductCardの呼び出しをループにしてjsonから呼び出し処理をする
+// todo: イメージが読み込めない問題をなんとかする
+// todo: ProductCardの呼び出しは下の方に責務を持たせるか？ -> Productcardをさらに呼び出すようのcomponentsを作成したい
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Header />
