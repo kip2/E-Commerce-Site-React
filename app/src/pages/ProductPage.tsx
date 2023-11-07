13c2bf351ff9904e7381ec836412b4e6260538c6
kip2 <tqa1no4@gmail.com>
2023-11-08 00:10:43 +0900

    todoコメントの追加

--- app/src/pages/ProductPage.tsx
+++ app/src/pages/ProductPage.tsx
@@ -1,6 +1,7 @@
 import Header from "../components/organisms/Header"
 import Categories from "../components/organisms/Categories"
 
+// todo: ソートとフィルタリング用のやつがいる
 export default function  ProductPage() {
     return (
         <>
