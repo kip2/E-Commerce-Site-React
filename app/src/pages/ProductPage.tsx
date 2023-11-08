beb9b4d46f6f1ee293d30d24c4babbde1375ba12
kip2 <tqa1no4@gmail.com>
2023-11-08 23:40:28 +0900

    todoコメント追加

--- app/src/pages/ProductPage.tsx
+++ app/src/pages/ProductPage.tsx
@@ -2,10 +2,11 @@ import Header from "../components/organisms/Header"
 import Categories from "../components/organisms/Categories"
 import PulldownMenus from "../components/molecules/PulldownMenus"
 
+// todo: sortとfilteringの内容を考える必要がある
+// todo: filterはカテゴリーの数だけ実装したいから、先に得たjsonデータからカテゴリーを配列に抜き出したい
 const sort = ["read", "blue", "green"]
 const filter = ["fishj", "meet", "beve"]
 
-// todo: ソートとフィルタリング用のやつがいる
 export default function  ProductPage() {
     return (
         <>
