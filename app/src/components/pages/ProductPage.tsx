d398d22992c381e1755c06af771284d5a793e8d5
kip2 <tqa1no4@gmail.com>
2023-11-09 01:10:30 +0900

    fix: 配列の要素を修正

--- app/src/components/pages/ProductPage.tsx
+++ app/src/components/pages/ProductPage.tsx
@@ -2,8 +2,8 @@ import ProductTemplate from "../template/ProductPageTemplate"
 
 // todo: sortとfilteringの内容を考える必要がある
 // todo: filterはカテゴリーの数だけ実装したいから、先に得たjsonデータからカテゴリーを配列に抜き出したい
-const sort = ["read", "blue", "green"]
-const filter = ["fish", "meet", "bread"]
+const sort = ["値段"]
+const filter = ["値段", "カテゴリー"]
 
 export default function  ProductPage() {
     return (
