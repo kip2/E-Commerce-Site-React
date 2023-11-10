5d754d55982fb50870f1e6f94f494338bb9addc6
kip2 <tqa1no4@gmail.com>
2023-11-10 21:19:16 +0900

    titleの移動

--- app/src/components/molecules/FavoriteCard.tsx
+++ app/src/components/molecules/FavoriteCard.tsx
@@ -1,8 +1,12 @@
+import "./Favorite.css"
+import Title from "../atoms/Title"
+
 const FavoriteCard = () => {
     return (
-        <>
+        <div className="favorite-card">
+            <Title title={"Favorite Items"}/>
             <h1>FavoriteCard</h1>
-        </>
+        </div>
     )
 }
 
--- app/src/components/template/FavoriteTemplate.tsx
+++ app/src/components/template/FavoriteTemplate.tsx
@@ -1,14 +1,10 @@
 import Header from "../organisms/Header";
-import Title from "../atoms/Title";
 import FavoriteCard from "../molecules/FavoriteCard";
 
 const FavoriteTemplate = () => {
     return (
         <>
             <Header />
-            <Title 
-                title={"Favorite Items"}
-            />
             <FavoriteCard />
         </>
     )
