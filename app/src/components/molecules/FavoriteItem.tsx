d3db3792d3bf931e915a75c47b1d2b0f383f52dc
kip2 <tqa1no4@gmail.com>
2023-11-10 22:00:51 +0900

    FavoriteItemにダミーデータ注入

--- app/src/components/molecules/FavoriteItem.tsx
+++ app/src/components/molecules/FavoriteItem.tsx
@@ -1,11 +1,21 @@
+import "./FavoriteItem.css"
 
+type FavoriteItemProps = {
+    imgUrl: string
+    name: string
+    price: number
+}
 
-const FavoriteCardItem = () => {
+const FavoriteItem:React.FC<FavoriteItemProps> = ({ imgUrl, name, price }) => {
     return(
-        <div className="">
-            <h1>FavoriteCardItem</h1>
-        </div>
+        <>
+            <div className="favorie-item">
+                <img src={imgUrl} />
+                <p className="name">{name}</p>
+                <p className="price">{price}~</p>
+            </div>
+        </>
     )
 }
 
-export default FavoriteCardItem
+export default FavoriteItem
--- app/src/components/organisms/FavoriteItems.tsx
+++ app/src/components/organisms/FavoriteItems.tsx
@@ -1,12 +1,23 @@
 import "./FavoriteItems.css"
 import Title from "../atoms/Title"
-import FavoriteCardItem from "../molecules/FavoriteItem"
+import FavoriteItem from "../molecules/FavoriteItem"
+
+const data = { 
+    "name": "Cyber Workstation",
+    "imgUrl": "./img/desktop1.png",
+    "price": 130896,
+    "description": "このデスクトップは、プロフェッショナルな映像編集や3Dレンダリングのために特別に設計されました。最新の高速プロセッサーと大容量のRAMを搭載しており、複雑なタスクでもスムーズに対応します。"
+}
 
 const FavoriteCard = () => {
     return (
         <div className="favorite-card">
             <Title title={"Favorite Items"}/>
-            <FavoriteCardItem />
+            <FavoriteItem 
+                imgUrl={data.imgUrl}
+                name={data.name}
+                price={data.price}
+            />
         </div>
     )
 }
