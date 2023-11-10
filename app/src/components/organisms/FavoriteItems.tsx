59559a3459764620313cdc53a4b74a6e39dc1bba
kip2 <tqa1no4@gmail.com>
2023-11-10 22:40:21 +0900

    favoriteページを配列の処理に変更

--- app/src/components/organisms/FavoriteItems.tsx
+++ app/src/components/organisms/FavoriteItems.tsx
@@ -2,22 +2,38 @@ import "./FavoriteItems.css"
 import Title from "../atoms/Title"
 import FavoriteItem from "../molecules/FavoriteItem"
 
-const data = { 
-    "name": "Cyber Workstation",
-    "imgUrl": "./img/desktop1.png",
-    "price": 130896,
-    "description": "このデスクトップは、プロフェッショナルな映像編集や3Dレンダリングのために特別に設計されました。最新の高速プロセッサーと大容量のRAMを搭載しており、複雑なタスクでもスムーズに対応します。"
-}
+const data = [
+    { 
+        "name": "Cyber Workstation",
+        "imgUrl": "./img/desktop1.png",
+        "price": 130896,
+        "description": "このデスクトップは、プロフェッショナルな映像編集や3Dレンダリングのために特別に設計されました。最新の高速プロセッサーと大容量のRAMを搭載しており、複雑なタスクでもスムーズに対応します。"
+    },
+    { 
+        "name": "Cyber Workstation",
+        "imgUrl": "./img/desktop1.png",
+        "price": 130896,
+        "description": "このデスクトップは、プロフェッショナルな映像編集や3Dレンダリングのために特別に設計されました。最新の高速プロセッサーと大容量のRAMを搭載しており、複雑なタスクでもスムーズに対応します。"
+    }
+]
 
 const FavoriteCard = () => {
     return (
         <div className="favorite-card">
             <Title title={"Favorite Items"}/>
-            <FavoriteItem 
-                imgUrl={data.imgUrl}
-                name={data.name}
-                price={data.price}
-            />
+            <div>
+                {
+                    data.map((item) => {
+                        return (
+                            <FavoriteItem
+                                imgUrl={item.imgUrl}
+                                name={item.name}
+                                price={item.price}
+                            />
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
