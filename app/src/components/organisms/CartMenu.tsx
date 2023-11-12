3fdb5213ef76c5b270a180b76ea4ac4e857c77bd
kip2 <tqa1no4@gmail.com>
2023-11-12 23:39:46 +0900

    CartMenuのボタンを要望通りに調整

--- app/src/components/organisms/CartMenu.tsx
+++ app/src/components/organisms/CartMenu.tsx
@@ -3,36 +3,22 @@ import Calculation from "../molecules/Calculation"
 import "./CartMenu.css"
 
 type CartMenuProps = {
-    productName: string
     price: number
 }
 
 // todo: あとで実装する
-const addFavoriteClick = () => {}
-// todo: あとで実装する
-const addCartClick = () => {}
-// todo: あとで実装する
-const goCartPage= () => {}
+const addPurchaseClick = () => {}
 
-const CartMenu: React.FC<CartMenuProps> = ({productName, price}) => {
+const CartMenu: React.FC<CartMenuProps> = ({price}) => {
     return (
         <div className="cart-menu">
-            <h3 className="cart-menu-title">{productName}</h3>
             <Calculation
                 price={price}
             />
             <div className="buttons">
                 <Button
-                    caption={"Add Favorite"}
-                    onButtonClick={addFavoriteClick}
-                />
-                <Button
-                    caption={"Add Cart"}
-                    onButtonClick={addCartClick}
-                />
-                <Button
-                    caption={"Go Cart Page"}
-                    onButtonClick={goCartPage}
+                    caption={"Purchase"}
+                    onButtonClick={addPurchaseClick}
                 />
             </div>
         </div>
--- app/src/components/template/Cart.tsx
+++ app/src/components/template/Cart.tsx
@@ -4,6 +4,13 @@ import CartItems from "../organisms/CartItems"
 import CartMenu from "../organisms/CartMenu"
 import "./Cart.css"
 
+const cartDummyData = { 
+        "name": "Home Essemtial PC",
+        "imgUrl": "./img/desktop2.png",
+        "price": 132364,
+        "description": "家庭用として最適なこのデスクトップは、日常の業務や学習、エンターテインメントに適しています。コンパクトなデザインと手頃な価格で、あらゆるユーザーにおすすめです。"
+}
+
 const Cart = () => {
     return(
         <>
@@ -11,7 +18,7 @@ const Cart = () => {
             <div className="order-field">
                 <CartItems />
                 <CartMenu 
-
+                    price={cartDummyData.price}
                 />
             </div>
             <FavoriteItems />
