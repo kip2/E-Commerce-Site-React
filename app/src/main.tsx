b9e2aeb6643a810586463fd51d1ba6825fe4b3b6
kip2 <tqa1no4@gmail.com>
2023-11-13 21:57:45 +0900

    Appファイルを作成

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -6,12 +6,14 @@ import ProductPage from './components/pages/ProductPage'
 import ProductInformationTemplate from './components/template/ProductInfomationTemplate'
 import FavoriteTemplate from './components/template/FavoriteTemplate'
 import CartTemplate from './components/template/CartTemplate'
+import App from './components/App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <App />
     {/* <ProductPage /> */}
     {/* <ProductInformationTemplate /> */}
     {/* <FavoriteTemplate /> */}
-    <CartTemplate />
+    {/* <CartTemplate /> */}
   </React.StrictMode>,
 )
