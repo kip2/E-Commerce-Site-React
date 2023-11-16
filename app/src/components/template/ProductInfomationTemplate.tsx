53d4abbf1bc85b0671a210cd7e1cde9b5eee25cd
kip2 <tqa1no4@gmail.com>
2023-11-16 21:13:42 +0900

    各IDのデータを受け取れるように変更

--- app/src/components/template/ProductInfomationTemplate.tsx
+++ app/src/components/template/ProductInfomationTemplate.tsx
@@ -10,40 +10,35 @@ type ProductInformationTemplateProps = {
 
 }
 
-const data = { 
-    "id": 1,
-    "name": "Cyber Workstation",
-    "imgUrl": "./img/desktop1.png",
-    "price": 130896,
-    "description": "このデスクトップは、プロフェッショナルな映像編集や3Dレンダリングのために特別に設計されました。最新の高速プロセッサーと大容量のRAMを搭載しており、複雑なタスクでもスムーズに対応します。"
-}
 
 const ProductInformationTemplate: React.FC<ProductInformationTemplateProps> = () => {
-    // const data = useContext(ProductContext)
+    const data = useContext(ProductContext)
 
-    // const { id } = useParams()
+    const { id } = useParams()
 
-    // if (!id || isNaN(parseInt(id))) {
-    //     return <div>無効なIDです</div>
-    // }
+    if (!id || isNaN(parseInt(id))) {
+        return <div>無効なIDです</div>
+    } 
 
-    // for (let i = 0; i < data.length; i++) {
-    //     if(!data[i].products.find(p => p.id === parseInt(id))) {
-    //         const product = (!data[i].products.find(p => p.id === parseInt(id)))
-    //     }
-    // }
-    // const product = data.find(category => category.products.find(p => p.id === parseInt(id)))
+    const product = data.find(data => data.id === parseInt(id))
+
+    if (!product) {
+        return <div>製品が見つかりません</div>
+    }
 
     return (
         <div className="outer">
             <Header/>
             <div className="information">
                 <InformationCard 
-                    {...data}
+                    name={product.name}
+                    imgUrl={product.imgUrl}
+                    price={product.price}
+                    description={product.description}
                 />
                 <InformationMenu 
-                    productName={data.name}
-                    price={data.price}
+                    productName={product.name}
+                    price={product.price}
                 />
             </div>
         </div>
